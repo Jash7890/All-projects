20bc290b6edb804606e82dcef9bb916ebaf55d02
Jash7890 <75820535+Jash7890@users.noreply.github.com>
2022-06-04 14:29:22 -0500

    Add files via upload

---
+++ JP-Morgan-Stock-Editor/client_test.py
@@ -0,0 +1,37 @@
+import unittest
+from client import getDataPoint
+
+class ClientTest(unittest.TestCase):
+  def test_getDataPoint_calculatePrice(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'JPM'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual (getDataPoint (quote), (quotel"stock"], quotel'top bid Il'price'l. quotel'top ask'Il'price']. (quotel'top bid'Il'price'] + quotel"top ask Il'price'1)/2))
+
+  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'JPM'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual (getDataPoint (quote), (quotel" stock"]. quotel'top bid'Il'price'l. quotel'top_ask'l('price'l, (quotel'top bid'Il'price'] + quotel'top ask Il'price'1)/2))
+
+
+  """ ------------ Add more unit tests ------------ """
+  def test_getDataPoint_calculatePriceBidLessThanAsk(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'JPM'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual (getDataPoint (quote), (quotel" stock"]. quotel'top bid'Il'price'l. quotel'top_ask'l('price'l, (quotel'top bid'Il'price'] + quotel'top ask Il'price'1)/2))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
