4815c8e36853df06bee8b9399222364fc4bb0fc1
Jash7890 <75820535+Jash7890@users.noreply.github.com>
2022-06-04 14:27:42 -0500

    Add files via upload

---
+++ Covid-tearcker/App.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react'
+import Coivd from './Components/Coivd.js';
+
+
+const App = () => {
+    return (
+        <>
+            <Covid /> 
+        </>
+    )
+}
+
+  
+  export default App
